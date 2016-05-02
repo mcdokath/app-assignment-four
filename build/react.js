078639829abb7c8959fda259da1a9f5a225e58b3
mcdokath <mcdokath@users.noreply.github.com>
2016-05-01 21:08:17 -0700

    Update and rename react.js to build/react.js

--- build/react.js
+++ build/react.js
@@ -6,7 +6,7 @@
  * Copyright 2013-present, Facebook, Inc.
  * All rights reserved.
  *
- * This source code is licensed under the BSD-style license found in the
+ * This source code is licensed under the BSD-style license found in thebuild/
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  *
